bad726d8f93e56b02fb1309a9cae66b533c44d08
N. D. Winterbourne <53401774+FaceND@users.noreply.github.com>
2023-05-03 18:02:38 +0700

    Update SumScore.java

--- SumScore.java
+++ SumScore.java
@@ -2,6 +2,7 @@ class SumScore { // 1
     public static void main (String[] args) {
         int score1 = 15;
         int score2 = 35;
-        int sum = score1+score2;
+        int sum = score1 + score2;
+        System.out.println("Sum of " + score1 + " and " + score2 + " is " + sum);
     }
 }
